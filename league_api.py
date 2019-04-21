ba0a454b19ccd1d86261b0d5d5fce6ebab15ba88
cschwirian <cschwirian@gmail.com>
2019-04-21 15:57:52 -0700

    Added generic winrate function as well as utility functions.

--- league_api.py
+++ league_api.py
@@ -8,6 +8,7 @@ with open( "config/league_api_key.txt", "r" ) as config:
 CURRENT_SEASON = "9"
 INVALID_CHAMPION = 1
 INVALID_SUMMONER = 2
+NO_MATCHES = 3
 
 
 QUEUE_IDS = { "ranked": 420, "draft": 400, "blind": 430, "flex": 440 }
@@ -17,15 +18,75 @@ CHAMPION_IDS = { 'aatrox': 266, 'thresh': 412, 'tryndamere': 23, 'gragas': 79, '
 SEASON_IDS = { "9": 13, "8": 11, "7": 9, "6": 7, "5": 5, "4": 3, "3": 1 }
 
 
+
+def get_winrate( summoner_name, champion, queue, season ):
+
+    winrate_message = "%s has a %.2f%% winrate with %s out of %d games"
+
+    try:
+        champion_id = CHAMPION_IDS[champion.lower()]
+    except:
+        return "That champion doesn't exist according to my records."
+
+    try:
+        if( queue ):
+            queue_id = QUEUE_IDS[queue.lower()]
+            winrate_message += " in " + queue.lower()
+        else:
+            queue_id = None
+        except:
+            return "I don't know about that queue."
+
+    try:
+        if( season ):
+            season_id = SEASON_IDS[season]
+            winrate_message += " for season " + season
+        else:
+            season_id = None
+    except:
+        return "I don't know about that season."
+
+    winrate_message += "."
+
+    account_id = get_account_id( summoner_name )
+
+    if( account_id == INVALID_SUMMONER ):
+        return "That player doesn't exist according to my records."
+
+    match_list = get_matches( account_id, queue_id, champion_id, season_id )
+
+    if( match_list == NO_MATCHES ):
+        return "%s has not played %s." % (summoner_name, champion.title())
+
+    winrate, num_matches = get_winrate_from_matches( match_list, summoner_name )
+    return winrate_message % (summoner_name, winrate, champion.title(), num_matches)
+
+
 def get_current_ranked_winrate( summoner_name, champion ):
 
     try:
         champion_id = CHAMPION_IDS[champion.lower()]
     except:
-        return INVALID_CHAMPION
+        return "That champion doesn't exist according to my records."
 
     season_id = SEASON_IDS[CURRENT_SEASON]
     queue_id = QUEUE_IDS["ranked"]
+
+    account_id = get_account_id( summoner_name )
+
+    if( account_id == INVALID_SUMMONER ):
+        return "That player doesn't exist according to my records."
+
+    match_list = get_matches( account_id, queue_id, champion_id, season_id )
+
+    if( match_list == NO_MATCHES ):
+        return "%s has not played %s this season." % (summoner_name, champion.title())
+
+    winrate, num_matches = get_winrate_from_matches( match_list, summoner_name )
+    return "%s has a %.2f%% winrate with %s out of %d games." % (summoner_name, winrate, champion.title(), num_matches)
+
+
+def get_account_id( summoner_name ):
 
     summoner_url = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/%s?api_key=%s"
     summoner_response = requests.get( summoner_url % (summoner_name, API_KEY) )
@@ -35,24 +96,42 @@ def get_current_ranked_winrate( summoner_name, champion ):
         return INVALID_SUMMONER
 
     summoner_json = json.loads( summoner )
-    account_id = summoner_json["accountId"]
 
-    # Grabs the first up to 100 matches played by the player to ensure the
-    # players has played the specified champion.
-    matches_url = "https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/%s?api_key=%s&beginIndex=%d&endIndex=%d&queue=%d&champion=%d&season=%d"
-    matches_response = requests.get( matches_url % (account_id, API_KEY, 0, 100, queue_id, champion_id, season_id) )
+    return summoner_json["accountId"]
+
+
+def get_matches( account_id, queue_id, champion_id, season_id ):
+
+    match_list = []
+
+    matches_url = "https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/"
+    matches_url += account_id
+    matches_url += "?api_key=%s" % API_KEY
+
+    if( queue_id != None ):
+        matches_url += "&queue=%s" % queue_id
+
+    if( champion_id != None ):
+        matches_url += "&champion=%s" % champion_id
+
+    if( season_id != None ):
+        matches_url += "&season=%s" % season_id
+
+    matches_url += "&beginIndex=%d&endIndex=%d"
+
+    matches_response = requests.get( matches_url % (0, 100) )
     matches = matches_response.text
 
     if( not "200" in str( matches_response ) ):
-        return "%s has not played %s this season." % (summoner_name, champion.title())
+        return NO_MATCHES
 
     matches_json = json.loads( matches )
-    match_list = matches_json["matches"]
+    match_list += matches_json["matches"]
 
     index = 100
 
     while( True ):
-        matches = requests.get( matches_url % (account_id, API_KEY, index, index + 100, queue_id, champion_id, season_id) ).text
+        matches = requests.get( matches_url % (index, index + 100) ).text
         matches_json = json.loads( matches )
         match_list += matches_json["matches"]
 
@@ -62,8 +141,12 @@ def get_current_ranked_winrate( summoner_name, champion ):
         index += 100
 
     if( len( match_list ) == 0 ):
-        return "%s has not played %s this season." % (summoner_name, champion.title())
+        return NO_MATCHES
 
+    return match_list
+
+
+def get_winrate_from_matches( match_list, summoner_name ):
 
     win_counter = 0
     match_url = "https://na1.api.riotgames.com/lol/match/v4/matches/%d?api_key=%s"
@@ -83,4 +166,8 @@ def get_current_ranked_winrate( summoner_name, champion ):
                 if( participant["stats"]["win"] ):
                     win_counter += 1
 
-    return "%s has a %.2f%% winrate with %s out of %d games." % (summoner_name, (win_counter / len( match_list ) * 100), champion.title(), len( match_list ))
+    num_matches = len( match_list )
+    win_percentage = win_counter / num_matches * 100
+
+    return win_percentage, num_matches
+    # return "%s has a %.2f%% winrate with %s out of %d games." % (summoner_name, win_percentage, champion.title(), num_matches)
