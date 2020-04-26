29efca3415b139cace7df16be79c97a7b6a489ba
Connor Schwirian <cschwirian@gmail.com>
2020-04-26 01:32:58 -0700

    idk lmao

--- kenny_bot.py
+++ kenny_bot.py
@@ -1,6 +1,5 @@
 # These are the dependecies. The bot depends on these to function, hence the name. Please do not change these unless your adding to them, because they can break the bot.
 import discord
-import asyncio
 import platform
 import random
 import requests
@@ -42,78 +41,81 @@ async def on_message( message ):
 
 # System Commands
 @client.command( pass_context = True)
-@asyncio.coroutine
-def __stop__( context ):
-    if( context.message.author.id == "131579027659030528" ):
-        yield from client.close()
+async def stop( context ):
+    if( context.message.author.id == 131579027659030528 ):
+        await client.close()
     else:
-        yield from context.channel.send( "Uh uh uh. You're not allowed to do that." )
+        await context.channel.send( "Uh uh uh. You're not allowed to do that." )
 
-@client.command( pass_context = True)
-@asyncio.coroutine
-def __restart__( context ):
-    if( context.message.author.id == "131579027659030528" ):
-        yield from client.run( DISCORD_API_KEY )
-    else:
-        yield from context.channel.send( "Uh uh uh. You're not allowed to do that." )
+# @client.command( pass_context = True)
+# async # def restart( context ):
+#     if( context.message.author.id == 131579027659030528 ):
+#         await client.run( DISCORD_API_KEY )
+#     else:
+#         await context.channel.send( "Uh uh uh. You're not allowed to do that." )
 
 @client.command( pass_context = True )
-@asyncio.coroutine
-def __get_emojis__( context ):
-    print( "Emojis for server {} ({}):".format( context.message.server.id, context.message.server.name ) )
+async def get_emojis( context ):
+    if( context.message.author.id == 131579027659030528 ):
+        print( "Emojis for server {} ({}):".format( context.message.server.id, context.message.server.name ) )
+    else:
+        emojis = context.message.server.emojis
+        for index in range( len( context.message.server.emojis ) ):
+            print( "    " + emojis[index].name + " - " + emojis[index].id )
 
-    emojis = context.message.server.emojis
-    for index in range( len( context.message.server.emojis ) ):
-        print( "    " + emojis[index].name + " - " + emojis[index].id )
-    yield
 
 
 
 # Commands
 @client.command( pass_context = True, description = "Says \"hey\". Pretty straightforward." )
-@asyncio.coroutine
-def hey( context ):
-    yield from context.channel.send( "hey" )
+async def hey( context ):
+    await context.channel.send( "hey" )
+
+@client.command( pass_context = True, description = "Says \"hey\", but with some pizazz." )
+async def heyy( context ):
+    await context.channel.send( "ayyyeee" )
+
+@client.command( pass_context = True )
+async def sixnine( context ):
+    await context.channel.send( "ayeeeeeeeee" )
 
 @client.command( pass_context = True, description = "Provides a link to the repository for kenny_bot." )
-@asyncio.coroutine
-def repo( *args ):
-    yield from context.channel.send( "https://github.com/cschwirian/kenny_bot" )
+async def repo( context ):
+    await context.channel.send( "https://github.com/cschwirian/kenny_bot" )
 
 @client.command( pass_context = True, description = "For when a message needs a little more of that je ne sais quoi." )
-@asyncio.coroutine
-def improve( context, *, message ):
+async def improve( context, *, message ):
     if( str( message ).lower() == "kenny"):
-        yield from context.channel.send( "Cannot be improved." )
+        await context.channel.send( "Cannot be improved." )
     else:
         message_str = str( message ).replace( "b", ":b:" ).replace( "B", ":b:" )
-        yield from context.channel.send( message_str )
+        await context.channel.send( message_str )
 
 # TODO: Random image
 @client.command( pass_context = True )
-@asyncio.coroutine
-def pic( context ):
+async def pic( context ):
     pic = random.choice( os.listdir( "./images/pics" ) )
-    yield from context.channel.send( file=File(open("./images/pics/" + pic, "rb")) )
+    await context.channel.send( file=File(open("./images/pics/" + pic, "rb")) )
 
 # TODO: Random image of Kenny - credit Miles
 @client.command( pass_context = True )
-@asyncio.coroutine
-def kennypic( context ):
+async def kennypic( context ):
     pic = random.choice( os.listdir( "./images/kenny" ) )
-    yield from context.channel.send( file=File(open("./images/kenny/" + pic, "rb")) )
+    await context.channel.send( file=File(open("./images/kenny/" + pic, "rb")) )
 
 # TODO: Kenny quote of the day
-@client.command()
-@asyncio.coroutine
-def quote():
-    yield from context.channel.send( "I've been getting Yasuo in ARAM a lot recently." )
+@client.command( pass_context = True )
+async def quote( context ):
+    await context.channel.send( "I've been getting Yasuo in ARAM a lot recently." )
 
 @client.command( pass_context = True )
-@asyncio.coroutine
-def chill_gif( context ):
+async def chill_gif( context ):
     gif = random.choice( os.listdir( "./gifs/chill" ) )
-    yield from context.channel.send( file=File(open("./gifs/chill/" + gif, "rb")) )
+    await context.channel.send( file=File(open("./gifs/chill/" + gif, "rb")) )
+
+@client.command( pass_context = True )
+async def random_r34( context ):
+    await context.channel.send( "https://rule34.xxx/index.php?page=post&s=view&id=%d" % random.randint( 1000000, 3573088 ) )
 
 # TODO: Music
 
@@ -122,8 +124,7 @@ def chill_gif( context ):
 
 # TODO: Dice roll and other math
 @client.command( pass_context = True, description = "Rolls some dice. Use the format \"~dice xdy\" where x is the number of y sided dice. d is d." )
-@asyncio.coroutine
-def roll( context, *, message ):
+async def roll( context, *, message ):
     try:
         message_str = str( message ).lower()
         split_message = message_str.split( "d" )
@@ -131,55 +132,52 @@ def roll( context, *, message ):
         num_sides = int( split_message[1] )
 
         if( num_dice > 64 ):
-            yield from context.channel.send( "I can't hold all those dice there, friend." )
+            await context.channel.send( "I can't hold all those dice there, friend." )
         elif ( num_sides > 1000000 ):
-            yield from context.channel.send( "That's an unnecessarily large number, friend." )
+            await context.channel.send( "That's an unnecessarily large number, friend." )
         else:
             total_roll = 0
             for die in range( num_dice ):
                 total_roll += random.randint( 1, num_sides )
 
-            yield from context.channel.send( "You rolled a %d." % total_roll )
+            await context.channel.send( "You rolled a %d." % total_roll )
 
     except:
-        yield from context.channel.send( "You messed up the roll somehow. None of my business." )
+        await context.channel.send( "You messed up the roll somehow. None of my business." )
 
-@client.command()
-@asyncio.coroutine
-def what():
-    yield from context.channel.send( "Hey, buddy. I'm Kenny. Type \"~hey\" or type \"~help\" to see what I can do." )
+@client.command( pass_context = True )
+async def what( context ):
+    await context.channel.send( "Hey, buddy. I'm Kenny. Type \"~hey\" or type \"~help\" to see what I can do." )
 
 @client.command( pass_context = True, description = "Retrieves a player's winrate with a specific champion. Use the form \"~wr summoner/champion/queue/season\". queue and season optional." )
-@asyncio.coroutine
-def wr( context, *, message ):
+async def wr( context, *, message ):
     try:
         message_split = message.split( "/" )
         summoner_name = message_split[0]
         champion = message_split[1]
 
         if( len( message_split ) == 2 ):
-            yield from context.channel.send( league_api.get_winrate( summoner_name, champion, "", "" ) )
+            await context.channel.send( league_api.get_winrate( summoner_name, champion, "", "" ) )
         elif( len( message_split ) == 3 ):
             queue = message_split[2]
-            yield from context.channel.send( league_api.get_winrate( summoner_name, champion, queue, "") )
+            await context.channel.send( league_api.get_winrate( summoner_name, champion, queue, "") )
         else:
             queue = message_split[2]
             season = message_split[3]
-            yield from context.channel.send( league_api.get_winrate( summoner_name, champion, queue, season) )
+            await context.channel.send( league_api.get_winrate( summoner_name, champion, queue, season) )
     except:
-        yield from context.channel.send( "Yeah, you messed that one up, buddy." )
+        await context.channel.send( "Yeah, you messed that one up, buddy." )
 
 @client.command( pass_context = True, description = "Retrieves a player's winrate with a specific champion for the current ranked season. Use the format \"wr summoner/champion\". Only works for NA." )
-@asyncio.coroutine
-def rwr( context, *, message ):
+async def rwr( context, *, message ):
     try:
         message_split = message.split( "/" )
         summoner_name = message_split[0]
         champion = message_split[1]
 
-        yield from context.channel.send( league_api.get_current_ranked_winrate( summoner_name, champion ) )
+        await context.channel.send( league_api.get_current_ranked_winrate( summoner_name, champion ) )
     except:
-        yield from context.channel.send( "Yeah, you messed that one up, buddy." )
+        await context.channel.send( "Yeah, you messed that one up, buddy." )
 
 # Main
 if( __name__ == "__main__" ):
--- league_api.py
+++ league_api.py
@@ -11,7 +11,10 @@ INVALID_SUMMONER = 2
 NO_MATCHES = 3
 
 
-QUEUE_IDS = { "ranked": 420, "draft": 400, "blind": 430, "flex": 440 }
+QUEUE_IDS = { "ranked": 420, "draft": 400, "blind": 430, "flex": 440,
+              "custom": 0, "snowdown1v1": 72, "snowdown2v2": 73, "urf": 76,
+              "aram": 450, "arurf": 900, "ttlranked": 470, "ttl": 460
+            }
 
 CHAMPION_IDS = { 'aatrox': 266, 'thresh': 412, 'tryndamere': 23, 'gragas': 79, 'cassiopeia': 69, 'aurelion sol': 136, 'ryze': 13, 'poppy': 78, 'sion': 14, 'annie': 1, 'jhin': 202, 'karma': 43, 'nautilus': 111, 'kled': 240, 'lux': 99, 'ahri': 103, 'olaf': 2, 'viktor': 112, 'anivia': 34, 'singed': 27, 'garen': 86, 'lissandra': 127, 'maokai': 57, 'morgana': 25, 'evelynn': 28, 'fizz': 105, 'heimerdinger': 74, 'zed': 238, 'rumble': 68, 'mordekaiser': 82, 'sona': 37, "kog'maw": 96, 'katarina': 55, 'lulu': 117, 'ashe': 22, 'karthus': 30, 'alistar': 12, 'darius': 122, 'vayne': 67, 'varus': 110, 'udyr': 77, 'leona': 89, 'jayce': 126, 'syndra': 134, 'pantheon': 80, 'riven': 92, "kha'zix": 121, 'corki': 42, 'azir': 268, 'caitlyn': 51, 'nidalee': 76, 'kennen': 85, 'galio': 3, 'veigar': 45, 'bard': 432, 'gnar': 150, 'malzahar': 90, 'graves': 104, 'vi': 254, 'kayle': 10, 'irelia': 39, 'lee sin': 64, 'illaoi': 420, 'elise': 60, 'volibear': 106, 'nunu': 20, 'twisted fate': 4, 'jax': 24, 'shyvana': 102, 'kalista': 429, 'dr. mundo': 36, 'ivern': 427, 'diana': 131, 'tahm kench': 223, 'brand': 63, 'sejuani': 113, 'vladimir': 8, 'zac': 154, "rek'sai": 421, 'quinn': 133, 'akali': 84, 'taliyah': 163, 'tristana': 18, 'hecarim': 120, 'sivir': 15, 'lucian': 236, 'rengar': 107, 'warwick': 19, 'skarner': 72, 'malphite': 54, 'yasuo': 157, 'xerath': 101, 'teemo': 17, 'nasus': 75, 'renekton': 58, 'draven': 119, 'shaco': 35, 'swain': 50, 'talon': 91, 'janna': 40, 'ziggs': 115, 'ekko': 245, 'orianna': 61, 'fiora': 114, 'fiddlesticks': 9, "cho'gath": 31, 'rammus': 33, 'leblanc': 7, 'soraka': 16, 'zilean': 26, 'nocturne': 56, 'jinx': 222, 'yorick': 83, 'urgot': 6, 'kindred': 203, 'miss fortune': 21, 'wukong': 62, 'blitzcrank': 53, 'shen': 98, 'braum': 201, 'xin zhao': 5, 'twitch': 29, 'master yi': 11, 'taric': 44, 'amumu': 32, 'gangplank': 41, 'trundle': 48, 'kassadin': 38, "vel'koz": 161, 'zyra': 143, 'nami': 267, 'jarvan iv': 59, 'ezreal': 81 }
 
